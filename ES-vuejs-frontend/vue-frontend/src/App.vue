ad010df520644ec02bfd29fa5ef5f78067b6b61e
PranayK27 <kekre.pranay@gmail.com>
2022-09-07 09:16:20 +0100

    frontend

---
+++ ES-vuejs-frontend/vue-frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Employees />
+  </div>
+</template>
+
+<script>
+import Employees from './components/Employees.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Employees
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ES-vuejs-frontend/vue-frontend/src/components/EmployeesList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class = "container">
+
+    <h1 class = "text-center"> Employees List</h1>
+
+    <table class = "table table-striped">
+      <thead>
+      <tr>
+        <th> Employee Id</th>
+        <th> Employee First Name</th>
+        <th> Employee Last</th>
+        <th> Employee Email</th>
+      </tr>
+
+      </thead>
+      <tbody>
+      <tr v-for="employee in employees" v-bind:key="employee.id">
+        <td> {{employee.id }}</td>
+        <td> {{employee.firstName }}</td>
+        <td> {{employee.lastName}}</td>
+        <td> {{employee.email}}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+import EmployeeService from '../services/EmployeeService';
+
+export default {
+  name: 'employees-list',
+  data(){
+    return {
+      employees: []
+    }
+  },
+  methods: {
+    getEmployees(){
+      EmployeeService.getEmployees().then((response) => {
+        this.employees = response.data;
+      });
+    }
+  },
+  created() {
+    this.getEmployees();
+  }
+}
+
+</script>
